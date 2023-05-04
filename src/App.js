477154d8081f2574fa27c018eb0d7912d63931d8
UdayRajput001 <uday2001rajput@gmail.com>
2023-05-04 18:37:07 +0530

    React_Demo_Movie

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import movies from './movie.json'
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <div className='main'>
+      {
+      movies.map((element, index) => {
+        return (
+        <Movie 
+        key={index}
+        title={element.Title} 
+        year={element.Year} 
+        img={element.Poster}
+        />
+        )
+      
+      })
+
+      }
+     </div>
+    </div>
+  );
+}
+
+export default App;
